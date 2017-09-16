e487249c95fc1838b48e0ba59f5d334345cbdd9c
Christopher rinehart <RinehartC20@gmail.com>
2017-09-15 23:03:39 -0400

    ignored unnecessary files actor moves

---
+++ ProjectAlpha/Assets/Scripts/Actor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+
+    public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    private void Movement()
+    {
+        var xAxis = Input.GetAxis("Horizontal");
+        var zAxis = Input.GetAxis("Vertical");
+
+        var spdTime = speed * Time.deltaTime;
+
+        Vector3 pos = transform.position;
+        if(xAxis != 0.0f || zAxis != 0.0f)
+            pos = new Vector3(transform.position.x + xAxis* spdTime,pos.y, transform.position.z + zAxis* spdTime);
+        transform.position = pos;
+    }
+}
---
+++ ProjectAlpha/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform actor;
+    public float height = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        transform.parent = actor;
+        transform.localPosition = new Vector3(0.0f,height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
